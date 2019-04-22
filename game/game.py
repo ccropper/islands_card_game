be3dbf813c8cdc6b64934823eb83677a78881edb
Caiqian Cropper <caiqian.wu@gmail.com>
2019-04-21 21:37:35 -0500

    started working on game initialization

--- game/game.py
+++ game/game.py
@@ -5,19 +5,26 @@ POSSIBLE_SUITS = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
 POSSIBLE_VALUES = range(1,14)
 
 VALUES_MAP = {
-    1: 'Ace',
-    2: 'Two',
-    3: 'Three',
-    4: 'Four',
-    5: 'Five',
-    6: 'Six',
-    7: 'Seven',
-    8: 'Eight',
-    9: 'Nine',
-    10: 'Ten',
-    11: 'Jack',
-    12: 'Queen',
-    13: 'King',
+    1: 'A',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: '10',
+    11: 'J',
+    12: 'Q',
+    13: 'K',
+}
+
+SUITS_MAP = {
+    'Hearts': '♡',
+    'Spades': '♤',
+    'Diamonds': '♢',
+    'Clubs': '♧',
 }
 
 class Card:
@@ -28,7 +35,7 @@ class Card:
         self.location = location
 
     def __repr__(self):
-        return f"{VALUES_MAP[self.value]} of {self.suit}"
+        return f"{VALUES_MAP[self.value]}{SUITS_MAP[self.suit]}"
 
 class Deck:
     def __init__(self):
@@ -42,7 +49,24 @@ class Deck:
     
     def shuffle(self):
         random.shuffle(self.cards)
+
+    def get_top_card(self):
+        return self.cards.pop(0)
     
     def __repr__(self):
        return f"a Deck of {len(self.cards)} cards."
     
+class Game:
+    def __init__(self, rows=3, columns=4):
+        self.game_deck = Deck()
+        self.game_deck.shuffle()
+        self.game_board = []
+        for row in range(rows):
+            self.game_board.append([])
+            for column in range(columns):
+                self.game_board[row].append(self.game_deck.cards.pop(0))
+
+    # need to display top card face up
+    # need to get player input on where to place top card next
+        # add top card to player's chosen location, on top of "stack"
+
